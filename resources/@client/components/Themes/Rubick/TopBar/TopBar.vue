1f8bfecadea07d1eca381ea82809c507cc7c6bdb
Iwan Hermawan <ihermawan227@gmail.com>
2026-03-27 09:56:04 +0700

    perbaikan

--- resources/@client/components/Themes/Rubick/TopBar/TopBar.vue
+++ resources/@client/components/Themes/Rubick/TopBar/TopBar.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue' 
+import { ref, computed } from 'vue'
 import Lucide from "@/components/Base/Lucide";
 import Breadcrumb from "@/components/Base/Breadcrumb";
-import { FormInput } from "@/components/Base/Form";
-import { Menu, Popover } from "@/components/Base/Headless";
-import logoUrll from "@/assets/images/logo-tds-1.png";
-import fakerData from "@/utils/faker";
-import _ from "lodash";
-import { TransitionRoot } from "@headlessui/vue";
+import { Menu } from "@/components/Base/Headless";
+import defaultLogoUrl from "@/assets/images/logo-tds-1.png";
+import agenLogoUrl from "@/assets/images/logo-proenergi.png";
 import axios from 'axios'
 import Swal from 'sweetalert2'
 import { useAuthStore } from '@/stores/auth'
@@ -23,10 +20,23 @@ const hideSearchDropdown = () => {
 
 const router = useRouter()
 const auth = useAuthStore()
-// --- computed untuk menampilkan nama user ---
+
 const userName = computed(() => auth.user?.name || 'Guest')
 const userEmail = computed(() => auth.user?.email || '-')
-//const userEmail = computed(() => auth.user?.email || '')
+
+const agenRoles = [13, 14, 15, 16]
+
+const isAgenRole = computed(() => {
+  return agenRoles.includes(Number(auth.user?.id_role))
+})
+
+const currentLogo = computed(() => {
+  return isAgenRole.value ? agenLogoUrl : defaultLogoUrl
+})
+
+const brandName = computed(() => {
+  return isAgenRole.value ? 'Agen Tri Daya Selaras' : 'Tri Daya Selaras'
+})
 
 async function onLogout() {
   try {
@@ -55,40 +65,36 @@ async function onLogout() {
 </script>
 
 <template>
-  <!-- BEGIN: Top Bar -->
   <div
     class="relative z-[51] flex h-[67px] items-center border-b border-slate-200"
   >
     <!-- BEGIN: Breadcrumb -->
     <Breadcrumb class="hidden mr-auto -intro-x sm:flex">
       <Breadcrumb.Link to="/">Application</Breadcrumb.Link>
-      <Breadcrumb.Link to="/" :active="true"> CrushedStone</Breadcrumb.Link>
+      <Breadcrumb.Link to="/" :active="true">
+        {{ brandName }}
+      </Breadcrumb.Link>
     </Breadcrumb>
     <!-- END: Breadcrumb -->
+
     <!-- BEGIN: Search -->
     <div class="relative mr-3 intro-x sm:mr-6">
-      <div class="relative hidden sm:block">
-        
-       
-      </div>
-     
-     
+      <div class="relative hidden sm:block"></div>
     </div>
-    <!-- END: Search  -->
-    <!-- BEGIN: Notifications -->
-   
-    <!-- END: Notifications  -->
+    <!-- END: Search -->
+
     <!-- BEGIN: Account Menu -->
     <Menu>
       <Menu.Button
-        class="block w-8 h-8 overflow-hidden rounded-full shadow-lg image-fit zoom-in intro-x"
+        class="flex items-center justify-center overflow-hidden rounded-full shadow-lg zoom-in intro-x"
       >
-      <img
-            alt="Tinker Tailwind HTML Admin Template"
-            class="w-8"
-            :src="logoUrll"
-          />
+        <img
+          alt="Application Logo"
+          class="h-10 w-auto max-w-[42px] object-contain"
+          :src="currentLogo"
+        />
       </Menu.Button>
+
       <Menu.Items class="w-56 mt-px text-white bg-primary">
         <Menu.Header class="font-normal">
           <div class="font-medium">{{ userName }}</div>
@@ -96,37 +102,44 @@ async function onLogout() {
             {{ userEmail }}
           </div>
         </Menu.Header>
+
         <Menu.Divider class="bg-white/[0.08]" />
+
         <Menu.Item
-  as="button"
-  class="hover:bg-white/5"
-  @click="router.push({ name: 'profile-overview-1' })"
->
-  <Lucide icon="User" class="w-4 h-4 mr-2" />
-  Profile
-</Menu.Item>
-<Menu.Item
+          as="button"
+          class="hover:bg-white/5"
+          @click="router.push({ name: 'profile-overview-1' })"
+        >
+          <Lucide icon="User" class="w-4 h-4 mr-2" />
+          Profile
+        </Menu.Item>
+
+        <Menu.Item
           v-if="auth.user?.id_role === 1"
           as="button"
           class="hover:bg-white/5"
           @click="() => router.push({ name: 'users' })"
         >
-          <Lucide icon="Edit" class="w-4 h-4 mr-2" /> Add Account
-   </Menu.Item>
-       
-   <Menu.Item class="hover:bg-white/5">
-          <Lucide icon="HelpCircle" class="w-4 h-4 mr-2" /> Help
+          <Lucide icon="Edit" class="w-4 h-4 mr-2" />
+          Add Account
         </Menu.Item>
+
+        <Menu.Item class="hover:bg-white/5">
+          <Lucide icon="HelpCircle" class="w-4 h-4 mr-2" />
+          Help
+        </Menu.Item>
+
         <Menu.Divider class="bg-white/[0.08]" />
+
         <Menu.Item
-        as="button"
-        @click="onLogout"
-        class="w-full text-left hover:bg-white/5 flex items-center px-4 py-2"
-      >
-        <Lucide icon="ToggleRight" class="w-4 h-4 mr-2" /> Logout
-      </Menu.Item>
+          as="button"
+          @click="onLogout"
+          class="w-full text-left hover:bg-white/5 flex items-center px-4 py-2"
+        >
+          <Lucide icon="ToggleRight" class="w-4 h-4 mr-2" />
+          Logout
+        </Menu.Item>
       </Menu.Items>
     </Menu>
   </div>
-  <!-- END: Top Bar -->
 </template>
