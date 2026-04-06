a16b3f43e5eee9808e37e55714cfebcffb0ab899
Iwan Hermawan <ihermawan227@gmail.com>
2026-04-06 09:45:41 +0700

    kurang validator user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -120,4 +121,6 @@ class UserController extends Controller
             'message' => 'Password berhasil direset'
         ]);
     }
+
+    
 }
